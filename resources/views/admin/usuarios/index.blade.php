f15d6acdeb620eaa7b860e55212fb6fff9bb5239
pabloperez38 <pablo.eluniversoweb@gmail.com>
2026-09-22 22:08:11 -0300

    arreglo pantalla

--- resources/views/admin/usuarios/index.blade.php
+++ resources/views/admin/usuarios/index.blade.php
@@ -8,16 +8,20 @@
 
     @include('admin.components.leftmenu')
 
-    <section class="page-title-dashboard">
-        <div class="themes-container">
-            <div class="row">
-                <div class="col-lg-12 col-md-12">
-                    <div class="title-dashboard">
-                        <div class="title-dash flex2">Usuarios</div>
+    <div class="dashboard__content">
+
+        <section class="page-title-dashboard">
+            <div class="themes-container">
+                <div class="row">
+                    <div class="col-lg-12 col-md-12">
+                        <div class="title-dashboard">
+                            <div class="title-dash flex2">Usuarios</div>
+                        </div>
                     </div>
                 </div>
             </div>
-        </div>
-    </section>
+        </section>
+
+    </div>
 
 @endsection
